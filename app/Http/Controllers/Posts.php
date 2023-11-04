9b1326df27fc14f1e186429944cc3d891a1ad157
PanoffVl <panoffvl@gmail.com>
2023-11-04 23:53:58 +0300

    Vtoroe sohraneniye

--- app/Http/Controllers/Posts.php
+++ app/Http/Controllers/Posts.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 // use Illuminate\Http\Request;
 
+use App\Models\Post;
+
 class Posts extends Controller
 {
     public function index()
     {
-      $posts = [['id'=>1], ['id'=>2]];
+      $posts = Post::all();
       return view('posts.index', [
         'posts' => $posts,
         'some' => 100
@@ -17,31 +19,31 @@ class Posts extends Controller
 
     // public function create()
     // {
-        
+
     // }
 
     // public function store(Request $request)
     // {
-        
+
     // }
 
     // public function show(string $id)
     // {
-        
+
     // }
 
     // public function edit(string $id)
     // {
-        
+
     // }
 
     // public function update(Request $request, string $id)
     // {
-        
+
     // }
 
     // public function destroy(string $id)
     // {
-        
+
     // }
 }
